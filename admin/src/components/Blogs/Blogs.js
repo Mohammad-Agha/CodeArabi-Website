2251a83b21db72238a2ca215b6bfdac4ab1479d7
Mohammad-Agha <hamadaagha123@gmail.com>
2020-11-27 08:25:12 +0200

    Admin panel search for a blog by tag

--- admin/src/components/Blogs/Blogs.js
+++ admin/src/components/Blogs/Blogs.js
@@ -8,14 +8,15 @@ import './Blogs.css'
 const Blogs = () => {
   const [offset, setOffset] = useState(0)
   const [blogs, setBlogs] = useState([])
-  const [perPage, setPerPage] = useState(7)
   const [fetchBlogs, setFetchBlogs] = useState(false)
   const [pageCount, setPageCount] = useState(1)
   const [alert, setAlert] = useState(false)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     let isMounted = true;
     const run = async () => {
+      const perPage = 7
       const response = await fetch(`http://localhost:5000/api/blog?page=${offset + 1}&limit=${perPage}`)
       const data = await response.json()
       if (isMounted) {
@@ -48,12 +49,32 @@ const Blogs = () => {
 
   const handlePageClick = (e) => setOffset(e.selected)
 
+  const searchBlog = async e => {
+    let search = e.target.value.trim()
+    setSearch(search)
+    if (search !== '') {
+      const response = await fetch(`http://localhost:5000/api/blog/search/${search}`)
+      const data = await response.json()
+      if (data.length === 0) {
+        setBlogs([])
+        return
+      }
+      setBlogs(data)
+    }
+    else {
+      setFetchBlogs(!fetchBlogs)
+    }
+  }
+
   return (
     <div className="div-wrapper">
       <div className="component-div">
         <span className="component-name">Blogs</span>
       </div>
       <Link className="link-btn" to='/admin/blogs/add'>Add blog</Link>
+      <div className="form-group">
+        <input type="text" onChange={searchBlog} placeholder="Search for a blog by tag" />
+      </div>
       {alert && <div className="show-alert">Blog Deleted</div>}
       <table className="table">
         <thead>
@@ -74,7 +95,7 @@ const Blogs = () => {
           ))}
         </tbody>
       </table>
-      <ReactPaginate
+      {search === '' ? <ReactPaginate
         previousLabel={"prev"}
         nextLabel={"next"}
         breakLabel={"..."}
@@ -86,7 +107,8 @@ const Blogs = () => {
         containerClassName={"pagination"}
         subContainerClassName={"pages pagination"}
         activeClassName={"active"}
-      />
+      /> : ''}
+
     </div>
   )
 }
